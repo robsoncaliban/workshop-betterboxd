b2cefddf8dc02454462cd57541bbea2779a10916
robsoncaliban <robsoncaliban@gmail.com>
2025-09-20 19:05:23 -0300

    chore: adiciona estrutura do projeto

---
+++ betterboxd/src/main/java/com/atoserobson/betterboxd/BetterboxdApplication.java
@@ -0,0 +1,13 @@
+package com.atoserobson.betterboxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterboxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterboxdApplication.class, args);
+	}
+
+}
---
+++ betterboxd/src/test/java/com/atoserobson/betterboxd/BetterboxdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atoserobson.betterboxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterboxdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
